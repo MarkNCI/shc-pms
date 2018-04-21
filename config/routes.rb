7f404a06be17abe036614e885bacbf799587cc0f
marknci <m.arkhayden@hotmail.com>
2018-04-21 16:58:36 +0000

    Final Commit

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   root "patients#index"
   get '/check', :controller=>'patients', :action=>'checkprice'
   post '/validate', :controller=>'patients', :action=>'cost'
+  get '/email', :controller=>'patients', :action=>'contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
